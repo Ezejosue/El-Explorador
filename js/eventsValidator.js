18e5570054dfdf791638313e64ee22363685e83e
Araniva27 <35879343+Araniva27@users.noreply.github.com>
2021-10-30 23:08:25 -0600

    inicio de validaciones

--- js/eventsValidator.js
+++ js/eventsValidator.js
@@ -1,6 +1,20 @@
+var validacionNombre = 0;
+var validacionLugar = 0;
+var validacionFecha = 0;
+
+//Validacion para longitud del campo de nombre del evento
 var txtNombreE = document.getElementById('txtNombreEvento');
 
 txtNombreE.addEventListener('focusout', function(evt){
-    var entrada =  document.getElementById('txtNombreEvento');
-    validarSoloLetras(entrada);
+   /* var entrada =  document.getElementById('txtNombreEvento');
+    if(validarSoloLetras(entrada) == 1){
+        swal("Exito", "El formato de campo es el correcto", "success");   
+    }*/
+    if(txtNombreE.value.length > 5){
+        swal("Verificar", "Se ha excedido la longitud del campo", "info");  
+        txtNombreE.style.borderColor = '#ff0000'
+    }else{
+        validacionNombre = 1;
+        txtNombreE.style.borderColor = null;
+    }
 },true)
--- js/validator.js
+++ js/validator.js
@@ -1,13 +1,16 @@
-var validacionNombre = 0;
-var validacionLugar = 0;
-var validacionFecha = 0;
-
 function validarSoloLetras(campo){
     var campoValidar = campo;
 
     if(campoValidar.value != ''){
-          
+          if(/^[a-zA-ZÀ-ÿ00f100d1]+(\s[a-zA-ZÀ-ÿ00f100d1])*[a-zA-ZÀ-ÿ00f100d1]+$/.test(campoValidar.value)){
+           // swal("Exito", "El formato de campo es el correcto", "success");   
+            return 1;
+          }else{
+            //swal("Error", "Error de campo incorrecto", "error");    
+            return 0;   
+          }
     }else{
-        swal("Error", "Debe de ingresar datos", "info");       
+        swal("Error", "Debe de ingresar datos", "info");    
+        return 0;   
     }
 }
