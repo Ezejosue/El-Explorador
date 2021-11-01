b16f71cffb7caef2da2bf60c31afecfeb8ebfa86
Araniva27 <35879343+Araniva27@users.noreply.github.com>
2021-10-31 23:24:19 -0600

    validacion de ingreso de eventos

--- js/eventsValidator.js
+++ js/eventsValidator.js
@@ -1,6 +1,35 @@
 var validacionNombre = 0;
 var validacionLugar = 0;
 var validacionFecha = 0;
+var validacionPrecio = 0;
+var validacionDescripcion = 0;
+
+const fecha = new Date();
+
+let dia = fecha.getDate();
+let mes = fecha.getMonth()+1;
+let año = fecha.getFullYear();
+
+let fechaMostrar = `${año}-${mes}-${dia}`;
+function mostrarCalendario(){
+    var div = document.getElementById('campoFecha');
+    var contenido = '';
+    contenido += `
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <label for="txtFechaRealizacion" class="form-label tituloCampo">Fecha de
+                realización</label>
+            <input type="date" class="form-control txt" id="txtFechaRealizacion"
+                    placeholder="Ingrese fecha de realización" min="${fechaMostrar}" required>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <label for="txtFechaInicio" class="form-label tituloCampo">Hora de
+                    realización</label>
+                <input type="time" class="form-control txt" id="txtFechaInicio"
+                    placeholder="Ingrese fecha de inicio" required>
+        </div>`;
+        div.innerHTML = contenido;
+}
+window.onload = mostrarCalendario
 
 //Validacion para longitud del campo de nombre del evento
 var txtNombreE = document.getElementById('txtNombreEvento');
@@ -10,11 +39,102 @@ txtNombreE.addEventListener('focusout', function(evt){
     if(validarSoloLetras(entrada) == 1){
         swal("Exito", "El formato de campo es el correcto", "success");   
     }*/
-    if(txtNombreE.value.length > 5){
-        swal("Verificar", "Se ha excedido la longitud del campo", "info");  
-        txtNombreE.style.borderColor = '#ff0000'
+    if(txtNombreE.value != ''){
+        if(txtNombreE.value.length > 150){
+            swal("Verificar", "Se ha excedido la longitud del campo", "info");  
+            txtNombreE.style.borderColor = '#ff0000'
+            validacionNombre = 0;
+        }else{
+            validacionNombre = 1;
+            txtNombreE.style.borderColor = null;
+        }
     }else{
-        validacionNombre = 1;
-        txtNombreE.style.borderColor = null;
+        swal("Verificar", "El campo no puede sere vacio", "info");  
+        txtNombreE.style.borderColor = '#ff0000';
+        validacionNombre = 0;
     }
 },true)
+
+
+//Validacion para lugar de realización dele evento
+var txtNombreL = document.getElementById('txtLugarRealizacion');
+
+txtNombreL.addEventListener('focusout', function(evt){  
+    if(txtNombreL.value != ''){
+        if(txtNombreL.value.length > 200){
+            swal("Verificar", "Se ha excedido la longitud del campo", "info");  
+            txtNombreL.style.borderColor = '#ff0000'
+            validacionLugar = 0;
+        }else{
+            validacionLugar = 1;
+            txtNombreL.style.borderColor = null;
+        }
+    }else{
+        swal("Verificar", "El campo no puede sere vacio", "info");  
+        txtNombreL.style.borderColor = '#ff0000';
+        validacionLugar = 0;
+    }
+},true)
+
+//Validacion de precio
+var txtPrecio = document.getElementById('txtPrecioEvento');
+
+txtPrecio.addEventListener('focusout',function(evt){
+    if(txtPrecio.value != ''){
+        if(/\d{1,3}(?:[.,]\d{3})*(?:[.,]\d{2})$/.test(txtPrecio.value)){
+            validacionPrecio = 1;
+            txtPrecio.style.borderColor = null;
+            //swal("Éxito", "Precio correcto", "success");
+        }
+        else{
+            swal("Verificar", "El precio no cumple con el formato establecido: su cantidad entera acompañada de un punto y dos decimaes", "error"); 
+            txtPrecio.style.borderColor = '#ff0000';
+            validacionPrecio = 0;
+
+        }
+    }
+    else{
+        swal("Verificar", "El campo no puede ser vacío", "info");
+        txtPrecio.style.borderColor = '#ff0000';
+        validacionPrecio = 0;
+    }
+})
+
+//Validación de descripción
+var txtDescripcion = document.getElementById('txtDescripcionEvento');
+
+txtDescripcion.addEventListener('focusout', function(evt){
+    if(txtDescripcion.value != ''){
+        if(txtDescripcion.value.length > 500){
+            swal("Verificar", "Se ha excedido la longitud del campo", "info");  
+            txtDescripcion.style.borderColor = '#ff0000';
+            validacionDescripcion = 0;
+        }
+        else{
+            validacionDescripcion = 1;
+            //swal("Éxito", "Descripción correcta", "success");
+        }
+    }
+    else{
+        swal("Verificar", "El campo no puede ser vacío", "info");
+        validacionDescripcion = 0;
+        txtDescripcion.style.borderColor = '#ff0000'
+    }
+})
+
+
+
+var btnAgregar = document.getElementById('botonAgregar');
+
+btnAgregar.addEventListener('click', function(evt){
+   if(validacionNombre == 1 && validacionLugar == 1 && validacionPrecio == 1 && validacionDescripcion == 1){
+    swal("Éxito", "Los datos han sido agregados con éxito", "success");
+    validacionNombre = 0;
+    validacionLugar = 0;
+    validacionFecha = 0;
+    validacionPrecio = 0;
+    validacionDescripcion = 0;
+   }else{
+        swal("Error", "Verificar que los campos tengan el formato correspondiente", "error");
+   }
+})
--- js/validator.js
+++ js/validator.js
@@ -1,16 +1,64 @@
-function validarSoloLetras(campo){
-    var campoValidar = campo;
+function validarSoloLetras(campo) {
+  var campoValidar = campo;
 
-    if(campoValidar.value != ''){
-          if(/^[a-zA-ZÀ-ÿ00f100d1]+(\s[a-zA-ZÀ-ÿ00f100d1])*[a-zA-ZÀ-ÿ00f100d1]+$/.test(campoValidar.value)){
-           // swal("Exito", "El formato de campo es el correcto", "success");   
-            return 1;
-          }else{
-            //swal("Error", "Error de campo incorrecto", "error");    
-            return 0;   
-          }
-    }else{
-        swal("Error", "Debe de ingresar datos", "info");    
-        return 0;   
+  if (campoValidar.value != '') {
+    if (/^[a-zA-ZÀ-ÿ00f100d1]+(\s[a-zA-ZÀ-ÿ00f100d1])*[a-zA-ZÀ-ÿ00f100d1]+$/.test(campoValidar.value)) {
+      // swal("Exito", "El formato de campo es el correcto", "success");   
+      return 1;
+    } else {
+      swal("Error", "El campo sólo acepta letras", "error");   
+      return 0;
     }
+  } else {
+    swal("Error", "Debe de ingresar datos", "info");
+    return 0;
+  }
+}
+
+function validarSoloNumeros(campo) {
+  var campoValidar = campo;
+
+  if (campoValidar.value != '') {
+    if (/^([0-9])*$/.test(campoValidar.value)) {
+      return 1;
+    } else {
+      swal("Error", "El campo sólo acepta números", "error");
+      return 0;
+    }
+  } else {
+    swal("Error", "Debe ingresar datos", "info");
+    return 0;
+  }
+}
+
+function validarCorreo(campo) {
+  var campoValidar = campo;
+
+  if (campoValidar.value != '') {
+    if (/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,4})+$/.test(campoValidar.value)) {
+      return 1;
+    } else {
+      swal("Error", "El formato de correo electrónico no es correcto", "error");
+      return 0;
+    }
+  } else {
+    swal("Error", "Debe ingresar datos", "info");
+    return 0;
+  }
+}
+
+function validarContraseña(campo){
+  var campoValidar = campo;
+
+  if(campoValidar.value != ''){
+    if(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{6,}$/.test(campoValidar.value)){
+      return 1;
+    } else{
+      swal("Error", "Debe de ingresar una contraseña con 6 caracteres como mínimo incluyendo una letra mayúscula, minúscula y un dígito", "error");
+      return 0;
+    }
+  }else{
+    swal("Error", "Debe ingresar datos", "info");
+    return 0;
+  }
 }
